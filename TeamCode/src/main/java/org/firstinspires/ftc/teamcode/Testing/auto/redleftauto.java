95d30196a2494811ef1380784db050799684521d
KjtheCool1 <kylehenderson1@outlook.com>
2023-12-02 08:12:07 -0800

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/auto/redleftauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/auto/redleftauto.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+//tests
 @Autonomous
 public class redleftauto extends LinearOpMode {
     DcMotor frontLeft;
